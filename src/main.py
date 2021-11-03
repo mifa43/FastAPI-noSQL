45969dbc194c3cc235e4248780c0f6c5cd40cb22
Mifa43 <mifa43kotez@gmail.com>
2021-11-03 11:42:08 +0100

    dodat je jednostavni fastapi file sa prethodnim tesom middleware-a, takodje pozvan test konekcije ka bazi

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import logging
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException, status
+import time
+from fastapi.responses import JSONResponse
+import pyarrow.parquet as pq
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import csv
+from crud import connection
+
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+
+app = FastAPI()
+
+@app.middleware('http')
+async def middleware_process(request: Request, call_next):
+    if request.headers["User-Agent"].find('Mobile') == -1:
+        print("PC user can use my api")
+        response = await call_next(request)
+        return response
+    else:
+        print("Phone user cant use my api")
+        return JSONResponse(content={
+            "message": "There is no phone response!"
+        }, status_code=401)
+
+@app.get("/")
+async def main():
+    conn = connection()
+    print(conn['database'])
+    return {"HEY": "John Doe"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, loop="asyncio")
