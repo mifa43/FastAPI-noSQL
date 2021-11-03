4cfd89a8655da7263a20ce32c0d2b32ca591a749
Mifa43 <mifa43kotez@gmail.com>
2021-11-03 12:30:29 +0100

    dodate su klase za ORM arango u init se nalazi parametar koji nije obavezan ali nam dopusta da ga pozivamo drugi  metodama

--- src/crud.py
+++ src/crud.py
@@ -2,12 +2,17 @@ from typing import Protocol
 from arango import ArangoClient
 from arango_orm import ConnectionPool
 
-def connection():
-    client = ArangoClient(
-        hosts=['http://localhost:8529'],
-        host_resolver='roundrobin'
-        )
-    #client1 = ArangoClient(protocol='http', host='localhost', port=8529)
-    test_db = client.db('test', username = "root", password = "mypassword")
-    db = ConnectionPool([client], "test", "root", "mypassword")
-    return {"database": "connected"}
+class ArangoConn():
+    def __init__(self, *db) -> None:
+        self.db = db
+    def test_connection(self):
+        client = ArangoClient(
+            hosts=['http://localhost:8529'],
+            host_resolver='roundrobin'
+            )
+        #client1 = ArangoClient(protocol='http', host='localhost', port=8529)
+        test_db = client.db('test', username = "root", password = "mypassword")
+        self.db = ConnectionPool([client], "test", "root", "mypassword")
+        return {"database": "connected"}
+    def create_collections(self):
+        pass
